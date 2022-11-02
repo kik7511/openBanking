4c95519db16cc05531557c4b54f001dc7324f1d3
김대겸 <kik7511@naver.com>
2022-11-02 14:52:10 +0900

    Merge branch 'main' of https://github.com/kik7511/openBanking.git into main

--- src/main/java/com/openBanking/modules/codegroup/CodeGroup.java
+++ src/main/java/com/openBanking/modules/codegroup/CodeGroup.java
@@ -7,4 +7,36 @@ public class CodeGroup {
 	private Integer ccgUseNy;
 	private Integer ccgDelNy;
 	
+	
+	public String getCcgSeq() {
+		return ccgSeq;
+	}
+	public void setCcgSeq(String ccgSeq) {
+		this.ccgSeq = ccgSeq;
+	}
+	public String getCcgCodegroup() {
+		return ccgCodegroup;
+	}
+	public void setCcgCodegroup(String ccgCodegroup) {
+		this.ccgCodegroup = ccgCodegroup;
+	}
+	public String getCodeGroupK() {
+		return codeGroupK;
+	}
+	public void setCodeGroupK(String codeGroupK) {
+		this.codeGroupK = codeGroupK;
+	}
+	public Integer getCcgUseNy() {
+		return ccgUseNy;
+	}
+	public void setCcgUseNy(Integer ccgUseNy) {
+		this.ccgUseNy = ccgUseNy;
+	}
+	public Integer getCcgDelNy() {
+		return ccgDelNy;
+	}
+	public void setCcgDelNy(Integer ccgDelNy) {
+		this.ccgDelNy = ccgDelNy;
+	}
+	
 }
